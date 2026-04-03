af460d0172548fb19dfcfcf3b5fa55a4228be7af
amarquez28 <zhukow28@gmail.com>
2026-04-02 19:41:19 -0700

    Competition last settings. Creating new branch from this setpoint.

---
+++ start_apriltag.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Activate the virtual environment
+source /home/ras/Ras/RAS-AprilTagDetection/.venv/bin/activate
+# Run the AprilTag Python program
+python /home/ras/Ras/RAS-AprilTagDetection/apriltag.py
